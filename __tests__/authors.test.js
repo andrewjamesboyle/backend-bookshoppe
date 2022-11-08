a685b5710fbe77d5c2a405524da556e1ac67e810
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-08 14:28:24 -0800

    authors test written

---
+++ __tests__/authors.test.js
@@ -0,0 +1,20 @@
+const setup = require('../data/setup');
+const app = require('../lib/app');
+const { request } = require('../lib/app');
+const pool = require('../lib/utils/pool');
+
+describe('author routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+    
+  it('should return a list of authors', async () => {
+    const res = await request(app).get('/authors');
+    expect(res.status).toEqual(200);
+  });
+    
+  afterAll(() => {
+    pool.end();
+  });
+});
+
